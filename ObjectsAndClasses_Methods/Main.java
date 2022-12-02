21e0e8daf4176626b56066acdbe8857165230a13
Антон Чиканов <chikanov.88@mail.ru>
2022-12-02 02:03:26 +0000

    Upload New File

---
+++ ObjectsAndClasses_Methods/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Arithmetic arithmetic = new Arithmetic(20,24);
+        arithmetic.print ();
+        arithmetic.sum();
+        arithmetic.increase();
+        arithmetic.max();
+        arithmetic.min();
+
+    }
+
+}
