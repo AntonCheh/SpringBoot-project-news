4423a79ad40bbe6c637055aaf378cedd04a19ee7
Anton Chikanov <chikanov.88@mail.ru>
2023-01-11 17:05:37 +0800

    start of the second part of homework

---
+++ ObjectsAndClasses_StaticFinal/src/src/Processor.java
@@ -0,0 +1,16 @@
+public final class Processor {
+
+        private final int frequency; // частота
+        private final int cores; // ядра
+        private final String manufacturer; // производитель
+        private final int weight;
+
+        public Processor(int frequency, int cores, String manufacturer, int weight) {
+        this.frequency = frequency;
+        this.cores = cores;
+        this.manufacturer = manufacturer;
+        this.weight = weight;
+    }
+
+
+}
