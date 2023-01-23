b7c0e183d10a952ae8f7653e1e72ee5ebf51e3de
Anton Chikanov <chikanov.88@mail.ru>
2023-01-23 17:46:06 +0800

    the second task, find all letters

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -3,10 +3,18 @@ public class Main {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
+        System.out.println();
+        for (int i = 1040; i < 1106; i++) {
+            char alphabet = (char) i;
+            System.out.println(i + " - " + alphabet);
+        }
 
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
     }
+
+
+
 }
