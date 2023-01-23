4da4622ba4745544497be2f3a23038772f0925ec
Anton Chikanov <chikanov.88@mail.ru>
2023-01-23 17:15:51 +0800

    fixed mistake in the first task

--- Primitives/IntOrInteger/src/Container.java
+++ Primitives/IntOrInteger/src/Container.java
@@ -1,11 +1,15 @@
 public class Container {
-    private Integer count;
+    private Integer count = 0;
+    // если я правильно понял, то Integer не может быть не задан
+    // либо нужно изменить Integer на примитив int
+
 
     public void addCount(int value) {
+
         count = count + value;
     }
 
     public int getCount() {
-        return count;
+           return count;
     }
 }
