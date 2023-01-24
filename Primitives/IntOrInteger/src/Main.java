e049ccf90355231f677f1fad3de5de35727d23b3
Anton Chikanov <chikanov.88@mail.ru>
2023-01-24 12:14:27 +0800

    добавил Ё

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -1,20 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        Container container = new Container();
+                Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
         System.out.println();
-        for (int i = 1040; i < 1106; i++) {
+
+        for (int i = 1040, j = 1105, g = 1238;  i < 1104; i++, j +=0, g += 0) {
+            // i = i + 1105;
+            // i = i + 1238;
             char alphabet = (char) i;
-            System.out.println(i + " - " + alphabet);
-        }
+            char alphabet2 = (char) j;
+            char alphabet3 = (char) g;
+
+        System.out.println(i + " - " + alphabet);
+            System.out.println(j + " - " + alphabet2);
+            System.out.println(g + " - " + alphabet3);
+
+    }
 
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
     }
-
 
 
 }
