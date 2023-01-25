a4d4693b6802e624cda783cda8492aa9399bcbc0
Anton Chikanov <chikanov.88@mail.ru>
2023-01-25 11:03:20 +0800

    правильное выполнение

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -5,16 +5,12 @@ public class Main {
         System.out.println(container.getCount());
         System.out.println();
 
-        for (int i = 1040, j = 1105, g = 1238;  i < 1104; i++, j +=0, g += 0) {
-            // i = i + 1105;
-            // i = i + 1238;
-            char alphabet = (char) i;
-            char alphabet2 = (char) j;
-            char alphabet3 = (char) g;
-
-        System.out.println(i + " - " + alphabet);
-            System.out.println(j + " - " + alphabet2);
-            System.out.println(g + " - " + alphabet3);
+        for (int i = 1; i < 65536; i++) {
+            if ('А' <= (char) i && (char) i <= 'я' || (char) i == 'ё' || (char) i == 'Ё') {
+                char alphabet = (char) i;
+                System.out.println(i + " - " + alphabet);
+            }
+        }
 
     }
 
@@ -25,4 +21,4 @@ public class Main {
     }
 
 
-}
+
