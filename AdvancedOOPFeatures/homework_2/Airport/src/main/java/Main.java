e2e2074da3a46c468e6f60f79b537a4cf70ea1da
Anton Chikanov <chikanov.88@mail.ru>
2023-10-24 16:07:13 +0800

    first attempt

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,7 +1,14 @@
-import com.skillbox.airport.Airport;
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import com.skillbox.airport.Airport;
+
+
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +17,27 @@ public class Main {
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+       Date timeNow = new Date();
+        Date twoHoursLate = calculateTwoHoursAfter (timeNow);
+
+        List<Flight> departingFlights = airport.getAllAircrafts().stream()
+                .filter(flight -> flight.getType() == Flight.Type.DEPARTURE).
+                filter(flight -> flight.getDate().before(twoHoursLate))
+                .collect(Collectors.toList());
+
+
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        return departingFlights;
     }
 
-}
+    private static Date calculateTwoHoursAfter(Date date) {
+        Calendar search = Calendar.getInstance();
+        search.setTime(date);
+
+        search.add(Calendar.HOUR_OF_DAY, 2);
+
+        Date twoHoursFromNow = search.getTime ();
+
+        return twoHoursFromNow;
+        }
+    }
