ec856a0f81a0c0fb701ce1bb4d4bbb18ebb7ade9
anton <chikanov.88@mail.ru>
2023-11-14 11:33:03 +0300

    second changes

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,42 +1,57 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
 
     public CustomerStorage() {
-
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data) {
+    public void addCustomer(String data) throws IllegalArgumentException {
+        final int EXPECTED_COMPONENTS = 4;
+        final int INDEX_NAME = 0;
+        final int INDEX_SURNAME = 1;
+        final int INDEX_EMAIL = 2;
+        final int INDEX_PHONE = 3;
 
+        String[] components = data.split("\\s+");
+//            String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+//            storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
 
-            final int INDEX_NAME = 0;
-            final int INDEX_SURNAME = 1;
-            final int INDEX_EMAIL = 2;
-            final int INDEX_PHONE = 3;
+        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+        String email = components[INDEX_EMAIL];
+        String phone = components[INDEX_PHONE];
 
-            String[] components = data.split("\\s+");
-            String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
-            storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+        if (components.length != EXPECTED_COMPONENTS) {
+            throw new IllegalArgumentException("Количество элементов более 4");
         }
-
-
-
-
+        if (!isValidPhoneNumber(phone)) {
+            throw new IllegalArgumentException("Неверный формат номера телефона");
+        }
+        if (!isValidEmail(email)) {
+            throw new IllegalArgumentException("Неверный формат электронной почты");
+        }
+        // storage.put(name, new Customer(name, phone, components[INDEX_EMAIL]));
+        storage.put(name, new Customer(name, phone, email));
+    }
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        return phoneNumber.matches("\\+7\\d{10}");
+    }
+    private boolean isValidEmail(String email) {
+        Pattern emailPattern = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$");
+        return emailPattern.matcher(email).matches();
+    }
     public void listCustomers() {
         storage.values().forEach(System.out::println);
     }
-
     public void removeCustomer(String name) {
         storage.remove(name);
     }
-
     public Customer getCustomer(String name) {
         return storage.get(name);
     }
-
     public int getCount() {
         return storage.size();
     }
