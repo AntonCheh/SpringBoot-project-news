60669cfd2eca56914546fdf40708781c39bdcc09
anton <chikanov.88@mail.ru>
2023-11-16 12:50:45 +0300

    third changes

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
-
+    static List<String> errorMessages;
     public CustomerStorage() {
         storage = new HashMap<>();
     }
-
     public void addCustomer(String data) throws IllegalArgumentException {
         final int EXPECTED_COMPONENTS = 4;
         final int INDEX_NAME = 0;
@@ -17,45 +18,66 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
-//            String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
-//            storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
 
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         String email = components[INDEX_EMAIL];
         String phone = components[INDEX_PHONE];
 
-        if (components.length != EXPECTED_COMPONENTS) {
-            throw new IllegalArgumentException("Количество элементов более 4");
+        try {
+            if (components.length != EXPECTED_COMPONENTS) {
+                throw new IllegalArgumentException("Неверное количество элементов. Ожидалось " + EXPECTED_COMPONENTS);
+            }
+
+          errorMessages = new ArrayList<>();
+
+            if (!isValidPhoneNumber(phone)) {
+                errorMessages.add("Неверный формат номера телефона");
+             // throw new IllegalArgumentException("Неверный формат номера телефона");
+            }
+            if (!isValidEmail(email)) {
+                errorMessages.add("Неверный формат электронной почты");
+              // throw new IllegalArgumentException("Неверный формат электронной почты");
+            }
+            if (!errorMessages.isEmpty()) {
+                throw new IllegalArgumentException(String.join(" ", errorMessages));
+            }
+            Main.queriesLogger.info("Добавлен новый клиент: " + name + ", email: " + email + ", телефон: " + phone);
+
+            // storage.put(name, new Customer(name, phone, components[INDEX_EMAIL]));
+            storage.put(name, new Customer(name, phone, email));
+        } catch  (IllegalArgumentException e) {
+
+            String errorMessage = "Ошибка при добавлении клиента: " + e.getMessage();
+            Main.errorsLogger.error(errorMessage, e);
+            System.err.println(errorMessage);
+
+//            String errorMessage = "Ошибка при добавлении клиента: " + e.getMessage();
+//            Main.logger.log(Level.ERROR, errorMessage, e);
+//            System.err.println(errorMessage);
         }
-        if (!isValidPhoneNumber(phone)) {
-            throw new IllegalArgumentException("Неверный формат номера телефона");
-        }
-        if (!isValidEmail(email)) {
-            throw new IllegalArgumentException("Неверный формат электронной почты");
-        }
-        // storage.put(name, new Customer(name, phone, components[INDEX_EMAIL]));
-        storage.put(name, new Customer(name, phone, email));
     }
-    private boolean isValidPhoneNumber(String phoneNumber) {
+            private boolean isValidPhoneNumber (String phoneNumber){
+
         return phoneNumber.matches("\\+7\\d{10}");
-    }
-    private boolean isValidEmail(String email) {
-        Pattern emailPattern = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$");
-        return emailPattern.matcher(email).matches();
-    }
-    public void listCustomers() {
-        storage.values().forEach(System.out::println);
-    }
-    public void removeCustomer(String name) {
-        storage.remove(name);
-    }
-    public Customer getCustomer(String name) {
-        return storage.get(name);
-    }
-    public int getCount() {
-        return storage.size();
-    }
-}
+            }
+            private boolean isValidEmail (String email){
+                Pattern emailPattern = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$");
+                return emailPattern.matcher(email).matches();
+            }
+            public void listCustomers () {
+                storage.values().forEach(System.out::println);
+            }
+            public void removeCustomer (String name){
+                storage.remove(name);
+            }
+            public Customer getCustomer (String name){
+                return storage.get(name);
+            }
+            public int getCount () {
+                return storage.size();
+            }
+        }
+
 
 
 
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,6 +1,12 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
+
+    static Logger logger;
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
@@ -9,7 +15,14 @@ public class Main {
             COMMAND_EXAMPLES;
     private static final String helpText = "Command examples:\n" + COMMAND_EXAMPLES;
 
+    static final Logger queriesLogger = LogManager.getLogger("queriesLogger");
+    static final Logger errorsLogger = LogManager.getLogger("errorsLogger");
+
     public static void main(String[] args) {
+         logger = LogManager.getRootLogger();
+//        logger.log(Level.WARN, "Сообщение об ошибке");
+//        logger.info("you made a mistake :");
+
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
 
@@ -18,27 +31,33 @@ public class Main {
             String[] tokens = command.split("\\s+", 2);
 
             try {
-                if (tokens[0].equals("add")) {
+                if (tokens[0].equalsIgnoreCase("add")) {
                     executor.addCustomer(tokens[1]);
+                    queriesLogger.info("Запрос к приложению: " + tokens[0]);
 
-                } else if (tokens[0].equals("list")) {
+                } else if (tokens[0].equalsIgnoreCase("list")) {
                     executor.listCustomers();
-                }
-                else if (tokens[0].equals("remove")) {
+                } else if (tokens[0].equalsIgnoreCase("remove")) {
                     executor.removeCustomer(tokens[1]);
-                }
-                else if (tokens[0].equals("count")) {
+                } else if (tokens[0].equalsIgnoreCase("count")) {
                     System.out.println("There are " + executor.getCount() + " customers");
-                } else if (tokens[0].equals("help")) {
+                } else if (tokens[0].equalsIgnoreCase("help")) {
                     System.out.println(helpText);
                 } else {
                     System.out.println(COMMAND_ERROR);
                 }
-            } catch (ArrayIndexOutOfBoundsException ex) {
-                System.out.println("Something wrong");
             }
+            catch (ArrayIndexOutOfBoundsException exception) {
+                //String errorMessage = exception.getMessage();
+                logger.log(Level.ERROR, "Ошибка выполнения команды: " + exception.getMessage(), exception);
+                System.err.println("Не введена команда");
 
-
+            }
+             catch (IllegalArgumentException e) {
+                //logger.log(Level.ERROR, "Ошибка выполнения команды: " + e.getMessage(), e);
+                 errorsLogger.error("Ошибка выполнения команды: " + e.getMessage(), e);
+            }
         }
     }
 }
+
